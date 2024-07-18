771a1242d25c2737d610186f36dc25fc5595aa49
saga-r-code <shah.sagar2006@gmail.com>
2024-07-18 19:42:42 +0530

    Updated database connection, route paths, and added notes route

--- database.js
+++ database.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-mongoose.connect('mongodb://localhost:27017')
+//Mongoose Connect to Database
+mongoose.connect('mongodb://localhost:27017/')
 
-app.use('api/auth', require('./routes/auth'))
-// app.use('api/notes', require('./routes/notes'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 
 app.get("/" , (req,res) =>{
     res.send("hello json")
-    req.send("heeeeeee")
 })
 
 app.listen(3000, ()=>{
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/' , (req, res)=>{
-    obj = {
-        a : "sagar",
-        number : 15,
-    }
-    res.json(obj)
+router.get("/", (req,res)=>{
+        res.json([])
 })
+
+
 
 module.exports = router
